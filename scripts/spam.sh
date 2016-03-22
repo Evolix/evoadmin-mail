c9ec9c36af964a7e89bfa1c71ca23b87e344e4e9
Gregory Colpart <gcolpart+git@evolix.fr>
2016-03-22 16:50:00 +0100

    upgrade hostname for spam infos

--- scripts/spam.sh
+++ scripts/spam.sh
@@ -19,24 +19,24 @@ cd /var/tmp
 
 sleep $[ $RANDOM / 1024 ]
 
-wget -q -t 3 http://zidane.evolix.net/spam/client.access -O $tmp_file
+wget -q -t 3 http://antispam00.evolix.net/spam/client.access -O $tmp_file
 cp $tmp_file /etc/postfix/client.access
 rm $tmp_file
 
-wget -q -t 3 http://zidane.evolix.net/spam/sender.access -O $tmp_file
+wget -q -t 3 http://antispam00.evolix.net/spam/sender.access -O $tmp_file
 cp $tmp_file /etc/postfix/sender.access
 rm $tmp_file
 
-wget -q -t 3 http://zidane.evolix.net/spam/recipient.access -O $tmp_file
+wget -q -t 3 http://antispam00.evolix.net/spam/recipient.access -O $tmp_file
 cp $tmp_file /etc/postfix/recipient.access
 rm $tmp_file
 
-wget -q -t 3 http://zidane.evolix.net/spam/header_kill -O $tmp_file
+wget -q -t 3 http://antispam00.evolix.net/spam/header_kill -O $tmp_file
 cp $tmp_file /etc/postfix/header_kill
 rm $tmp_file
 
-wget -q -t 3 http://zidane.evolix.net/spam/sa-blacklist.access  -O sa-blacklist.access
-wget -q -t 3 http://zidane.evolix.net/spam/sa-blacklist.access.md5  -O $tmp_file
+wget -q -t 3 http://antispam00.evolix.net/spam/sa-blacklist.access  -O sa-blacklist.access
+wget -q -t 3 http://antispam00.evolix.net/spam/sa-blacklist.access.md5  -O $tmp_file
 if md5sum -c $tmp_file > /dev/null && [ -s sa-blacklist.access ] ; then
         cp sa-blacklist.access /etc/postfix/sa-blacklist.access
 fi
@@ -48,8 +48,8 @@ rm $tmp_file
 /usr/sbin/postmap hash:/etc/postfix/recipient.access
 /usr/sbin/postmap -r hash:/etc/postfix/sa-blacklist.access
 
-wget -q -t 3 http://zidane.evolix.net/spam/spamd.cidr  -O spamd.cidr
-wget -q -t 3 http://zidane.evolix.net/spam/spamd.cidr.md5  -O $tmp_file
+wget -q -t 3 http://antispam00.evolix.net/spam/spamd.cidr  -O spamd.cidr
+wget -q -t 3 http://antispam00.evolix.net/spam/spamd.cidr.md5  -O $tmp_file
 if md5sum -c $tmp_file > /dev/null && [ -s spamd.cidr ] ; then
         cp spamd.cidr /etc/postfix/spamd.cidr
 fi
@@ -74,13 +74,13 @@ cd $tmp
 
 # ClamAV
 #for file in scam.ndb.gz phish.ndb.gz MSRBL-SPAM.ndb MSRBL-Images.hdb malware.com.br.ndb; do
-#    getfile http://zidane.evolix.net/spam/$file $file
+#    getfile http://antispam00.evolix.net/spam/$file $file
 #done
 #chown -R clamav:clamav /var/lib/clamav
 #/usr/sbin/invoke-rc.d clamav-daemon reload-database > /dev/null
 
 # SpamAssassin
-wget -q -t 3 http://zidane.evolix.net/spam/evolix_rules.cf -O evolix_rules.cf
+wget -q -t 3 http://antispam00.evolix.net/spam/evolix_rules.cf -O evolix_rules.cf
 cp evolix_rules.cf /etc/spamassassin
 /etc/init.d/spamassassin reload > /dev/null
 
