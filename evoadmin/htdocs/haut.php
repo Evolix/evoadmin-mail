05d2f8588ffb0e712e1bbc899e6cae43c7a39208
Victor Laborie <victor@laborie.io>
2017-10-12 20:52:32 +0200

    Fix vendor path

--- evoadmin/htdocs/haut.php
+++ evoadmin/htdocs/haut.php
@@ -3,14 +3,14 @@
     <head>
         <title><?php print $conf['html']['title']; ?></title>
         <link type="text/css" rel="stylesheet" href="styles.css"/>
-        <link type="text/css" rel="stylesheet" href="vendor/bootstrap/bootstrap.min.css"/>
-        <link type="text/css" rel="stylesheet" href="vendor/datatables/datatables.bootstrap.min.css"/>
+        <link type="text/css" rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css"/>
+        <link type="text/css" rel="stylesheet" href="vendor/datatables/css/datatables.bootstrap.min.css"/>
         <link type="image/ico" rel="icon" href="favicon.ico"/>
 
-        <script type="text/javascript" src="vendor/jquery/jquery.min.js"></script>
-        <script type="text/javascript" src="vendor/bootstrap/bootstrap.min.js"></script>
-        <script type="text/javascript" src="vendor/datatables/jquery.datatables.min.js"></script>
-        <script type="text/javascript" src="vendor/datatables/datatables.bootstrap.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery/js/jquery.min.js"></script>
+        <script type="text/javascript" src="vendor/bootstrap/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="vendor/datatables/js/jquery.datatables.min.js"></script>
+        <script type="text/javascript" src="vendor/datatables/js/datatables.bootstrap.min.js"></script>
     </head>
 <body>
     <div class="page-wrap">
