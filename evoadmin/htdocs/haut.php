657a2395661c5d7ae64ab9ab35f51e0787e09852
Victor Laborie <victor@laborie.io>
2017-10-12 20:14:33 +0200

    Move external scripts/styles into vendor dir

--- evoadmin/htdocs/haut.php
+++ evoadmin/htdocs/haut.php
@@ -2,15 +2,15 @@
 <html lang="fr">
     <head>
         <title><?php print $conf['html']['title']; ?></title>
-        <link type="text/css" rel="stylesheet" href="styles/bootstrap.min.css"/>
         <link type="text/css" rel="stylesheet" href="styles/main.css"/>
-        <link type="text/css" rel="stylesheet" href="styles/dataTables.bootstrap.min.css"/>
-        <link type="image/ico" rel="icon" href="img/favicon.ico" />
+        <link type="text/css" rel="stylesheet" href="vendor/bootstrap/bootstrap.min.css"/>
+        <link type="text/css" rel="stylesheet" href="vendor/datatables/datatables.bootstrap.min.css"/>
+        <link type="image/ico" rel="icon" href="img/favicon.ico"/>
 
-        <script type="text/javascript" src="scripts/jquery.min.js"></script>
-        <script type="text/javascript" src="scripts/bootstrap.min.js"></script>
-        <script type="text/javascript" src="scripts/jquery.dataTables.min.js"></script>
-        <script type="text/javascript" src="scripts/dataTables.bootstrap.min.js"></script>
+        <script type="text/javascript" src="vendor/jquery/jquery.min.js"></script>
+        <script type="text/javascript" src="vendor/bootstrap/bootstrap.min.js"></script>
+        <script type="text/javascript" src="vendor/datatables/jquery.datatables.min.js"></script>
+        <script type="text/javascript" src="vendor/datables/datatables.bootstrap.min.js"></script>
     </head>
 <body>
     <div class="page-wrap">
