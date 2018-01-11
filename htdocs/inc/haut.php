8938ed91da783dc127325ebe99f9faf686056721
Victor LABORIE <vlaborie@evolix.fr>
2018-01-12 00:29:23 +0100

    Use CDN for vendor with integrity check and same-origin policy

--- htdocs/inc/haut.php
+++ htdocs/inc/haut.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="fr">
     <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+        <meta name="robots" content="noindex,nofollow"/>
+        <meta name="referrer" content="same-origin"/>
         <title><?php print $conf['html']['title']; ?></title>
-        <link type="text/css" rel="stylesheet" href="css/main.css"/>
-        <link type="text/css" rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css"/>
-        <link type="text/css" rel="stylesheet" href="vendor/datatables/css/datatables.bootstrap.min.css"/>
-        <link type="image/ico" rel="icon" href="favicon.ico"/>
-
-        <script type="text/javascript" src="vendor/jquery/js/jquery.min.js"></script>
-        <script type="text/javascript" src="vendor/bootstrap/js/bootstrap.min.js"></script>
-        <script type="text/javascript" src="vendor/datatables/js/jquery.datatables.min.js"></script>
-        <script type="text/javascript" src="vendor/datatables/js/datatables.bootstrap.min.js"></script>
+        <link rel="stylesheet" href="css/main.css" type="text/css"/>
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha256-916EbMg70RQy9LHiGkXzG8hSg9EdNy97GazNG/aiY1w=" crossorigin="anonymous"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.16/css/dataTables.bootstrap.min.css" integrity="sha256-PbaYLBab86/uCEz3diunGMEYvjah3uDFIiID+jAtIfw=" crossorigin="anonymous"/>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha256-U5ZEeKfGNOja007MMD3YBI0A3OSZOQbeG6z2f2Y0hu8=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.16/js/jquery.dataTables.min.js" integrity="sha256-qcV1wr+bn4NoBtxYqghmy1WIBvxeoe8vQlCowLG+cng=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.16/js/dataTables.bootstrap.min.js" integrity="sha256-X/58s5WblGMAw9SpDtqnV8dLRNCawsyGwNqnZD0Je/s=" crossorigin="anonymous"></script>
     </head>
 <body>
     <div class="page-wrap">
