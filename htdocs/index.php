b4cb5772ac495801e63235d5cd4aeff34b28e407
Victor LABORIE <vlaborie@evolix.fr>
2018-09-25 16:31:18 +0200

    Rename class

--- htdocs/index.php
+++ htdocs/index.php
@@ -4,13 +4,7 @@ session_name('EVOADMIN_SESS');
 session_start();
 
 spl_autoload_register(function ($class) {
-    if (file_exists("lib/$class.php")) {
-        require_once("lib/$class.php");
-    }
-    $class = strtolower($class);
-    if (file_exists("lib/class.$class.php")) {
-        require_once("lib/class.$class.php");
-    } 
+    if (file_exists("lib/$class.php")) { require_once("lib/$class.php"); }
 });
 
 DefaultController::init();
